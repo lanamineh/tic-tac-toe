fd772b9580d8f89f2aa8cce63bd0c1a513facb1e
lanamineh <33867817+lanamineh@users.noreply.github.com>
2023-04-10 18:00:11 +0100

    Broken everything by moving stuff into functions

--- main/main.c
+++ main/main.c
@@ -8,20 +8,89 @@
 #include "esp_log.h"
 #include "commands.h"
 
-#define PIN_NUM_MISO 9
-#define PIN_NUM_MOSI 10
-#define PIN_NUM_CLK 1
-#define PIN_NUM_CS 2
-#define PIN_NUM_DC 3
+#define PIN_MISO 9
+#define PIN_MOSI 10
+#define PIN_CLK 1
+#define PIN_CS 2
+#define PIN_DC 3
+
+/* spi_device_handle_t initialise_spi() */
+/* { */
+/*     ESP_LOGI("main", "Initializing bus SPI%d...", SPI2_HOST+1); */
+/*     // Set the correct IO pins */
+/*     spi_bus_config_t buscfg = { */
+/*         .miso_io_num = PIN_MISO, */
+/*         .mosi_io_num = PIN_MOSI, */
+/*         .sclk_io_num = PIN_CLK, */
+/*     }; */
+/*     // Initialise the SPI bus (host) */
+/*     esp_err_t ret = spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_DISABLED); */
+/*     ESP_ERROR_CHECK(ret); */
+/*     ESP_LOGI("main", "SPI setup return code = %d", ret); */
+
+/*     // Configure the device */
+/*     spi_device_interface_config_t devcfg = { */
+/*         .clock_speed_hz = 10*1000*1000,  */
+/*         .mode = 0,                          */
+/*         .spics_io_num = PIN_CS,      */
+/*         .queue_size = 1,                 */
+/*     }; */
+/*     spi_device_handle_t device; */
+/*     ret = spi_bus_add_device(SPI2_HOST, &devcfg, &device);  */
+/*     ESP_ERROR_CHECK(ret); */
+/*     ESP_LOGI("main", "SPI device setup return code = %d", ret); */
+
+/*     return device; */
+/* } */
+
+void write_command(spi_device_handle_t device, uint8_t cmd)
+{
+    gpio_set_level(PIN_DC, 0);
+    spi_transaction_t t = {
+	//.flags = SPI_TRANS_USE_TXDATA,
+	.length = 8,
+	//.tx_data = &cmd,
+	.tx_buffer = &cmd
+    };
+    spi_device_polling_transmit(device, &t); 
+}
+
+void delay_ms(unsigned time)
+{
+    vTaskDelay(time / portTICK_PERIOD_MS);
+}
+
+void initialise_display(spi_device_handle_t device)
+{
+    write_command(device, SWRESET);
+    delay_ms(120);
+
+    write_command(device, SLPOUT);
+    delay_ms(120);
+
+    write_command(device, DISPON);
+}
+
+void write_data(spi_device_handle_t device, uint8_t * data, size_t len)
+{
+    //gpio_set_level(PIN_DC, 1);
+    spi_transaction_t t = {
+	.length = len*8,
+	.tx_buffer = data,
+    };
+    spi_device_polling_transmit(device, &t);
+    //ESP_LOGI("main", "DC pin... %d", gpio_get_level(PIN_DC));
+}
+
 
 void app_main(void)
 {
     ESP_LOGI("main", "Initializing bus SPI%d...", SPI2_HOST+1);
     // Set the correct IO pins
     spi_bus_config_t buscfg = {
-        .miso_io_num = PIN_NUM_MISO,
-        .mosi_io_num = PIN_NUM_MOSI,
-        .sclk_io_num = PIN_NUM_CLK,
+        .miso_io_num = PIN_MISO,
+        .mosi_io_num = PIN_MOSI,
+        .sclk_io_num = PIN_CLK,
     };
     // Initialise the SPI bus (host)
     esp_err_t ret = spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_DISABLED);
@@ -32,74 +101,45 @@ void app_main(void)
     spi_device_interface_config_t devcfg = {
         .clock_speed_hz = 10*1000*1000, 
         .mode = 0,                         
-        .spics_io_num = PIN_NUM_CS,     
+        .spics_io_num = PIN_CS,     
         .queue_size = 1,                
     };
     spi_device_handle_t device;
     ret = spi_bus_add_device(SPI2_HOST, &devcfg, &device); 
     ESP_ERROR_CHECK(ret);
     ESP_LOGI("main", "SPI device setup return code = %d", ret);
-
+    
     // Set up the D/C pin as a general input output pin
     gpio_config_t io_conf = {
-	.pin_bit_mask = (1ULL << PIN_NUM_DC),
+	.pin_bit_mask = (1ULL << PIN_DC),
 	.mode = GPIO_MODE_OUTPUT,
 	.pull_up_en = 0,
 	.pull_down_en = 1,
 	.intr_type = GPIO_INTR_DISABLE
     };
-    gpio_config(&io_conf);    
-    gpio_set_level(PIN_NUM_DC, 0);  // Only transmit commands
-    
-    // Transmit some data
-    uint8_t data = SWRESET;
-    //const char * data = "Hi";
-    spi_transaction_t t = {
-	.length = 8,  // Length of data * num bits in a byte
-	.tx_buffer = &data,
-    };
-    spi_device_polling_transmit(device, &t); 
-    vTaskDelay(120 / portTICK_PERIOD_MS);
+    gpio_config(&io_conf);
 
-    data = SLPOUT;
-    spi_device_polling_transmit(device, &t);
-    vTaskDelay(120 / portTICK_PERIOD_MS);
+    //spi_device_handle_t device = initialise_spi();
+    initialise_display(device);
 
-    // Set 18-bit pixel mode
-    /* data = COLMOD; */
-    /* spi_device_polling_transmit(device, &t); */
-    /* gpio_set_level(PIN_NUM_DC, 1); */
-    /* data = 0x06; */
-    /* spi_device_polling_transmit(device, &t); */
-
-    gpio_set_level(PIN_NUM_DC, 0);
-    data = DISPON;
-    spi_device_polling_transmit(device, &t); 
-
-    // IMPORTANT: This doesn't work before display is on
-    // Try to get one row
-    data = RASET; 
-    spi_device_polling_transmit(device, &t);
-    gpio_set_level(PIN_NUM_DC, 1);
-    uint8_t y_vals[] = {0x00, 50, 0x00, 52};
-    t.length = 4*8;
-    t.tx_buffer = y_vals;
-    spi_device_polling_transmit(device, &t);
-    // Enable write to ram
-    gpio_set_level(PIN_NUM_DC, 0);
-    data = RAMWR;
-    t.length = 8;
-    t.tx_buffer = &data;
-    spi_device_polling_transmit(device, &t);
+    /* write_command(device, RASET); */
+    /* // IMPORTANT: This doesn't work before display is on */
+    /* // Try to get one row */
+    /* uint8_t y_vals[] = {0x00, 50, 0x00, 52}; */
+    /* write_data(device, y_vals); */
+    write_command(device, RAMWR);
     // Write pixels
-    gpio_set_level(PIN_NUM_DC, 1);
-    uint8_t color[] = {0xff, 0xff, 0xff};
-    t.length = 3*8;
-    t.tx_buffer = color;
-    for (int i = 0; i < 1000; i++) {
-	spi_device_polling_transmit(device, &t);
+    gpio_set_level(PIN_DC, 1);
+    delay_ms(10);
+    ESP_LOGI("main", "DC pin... %d", gpio_get_level(PIN_DC));
+
+    uint8_t color[] = {0x00, 0xff, 0x00};
+    for (int i = 0; i < 100; i++) {
+	write_data(device, color, sizeof color);
     }
 
+
+    
     // To display
     // CASET can go from 2 to 129
     // RASET can go from 1 to 128 
